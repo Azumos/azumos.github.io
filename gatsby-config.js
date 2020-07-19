44eca8a40118adf7c056297c1deda03f11ce8e9e
welvin21 <wewe2712@connect.hku.hk>
2020-07-19 13:38:16 +0800

    Change page metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Azumos`,
+    description: `Azumos official website.`,
+    author: `Welvin Bun <welvin07@gmail.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
