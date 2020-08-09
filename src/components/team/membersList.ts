70a81014870816efbcf6508f6fb98b2e0bcf92ca
welvin21 <wewe2712@connect.hku.hk>
2020-08-09 17:37:59 +0800

    Complete members pictures

--- src/components/team/membersList.ts
+++ src/components/team/membersList.ts
@@ -1,12 +1,54 @@
 import { IMember } from "../../types";
 import welvin from "../../assets/images/members/welvin.png";
-import placeholder from "../../assets/images/members/placeholder.png";
+import kartikay from "../../assets/images/members/kartikay.png";
+import ammar from "../../assets/images/members/ammar.png";
+import louis from "../../assets/images/members/louis.png";
+import ken from "../../assets/images/members/ken.png";
+import regina from "../../assets/images/members/regina.png";
+import levinna from "../../assets/images/members/levinna.png";
+import william from "../../assets/images/members/william.png";
 
 export const membersList: IMember[] = [
   {
-    name: "person A",
-    role: "person A's role",
-    picture: placeholder,
+    name: "Ammar Sohail",
+    role: "Structural design specialist",
+    picture: ammar,
+    description: "",
+  },
+  {
+    name: "Immanuel William S.",
+    role: "E&M design specialist",
+    picture: william,
+    description: "",
+  },
+  {
+    name: "Kartikay Rana",
+    role: "Team leader",
+    picture: kartikay,
+    description: "",
+  },
+  {
+    name: "Levinna Natalia",
+    role: "Business analyst",
+    picture: levinna,
+    description: "",
+  },
+  {
+    name: "Louis Brighton",
+    role: "BIM specialist",
+    picture: louis,
+    description: "",
+  },
+  {
+    name: "Mikael Ken Slamet",
+    role: "Market analyst",
+    picture: ken,
+    description: "",
+  },
+  {
+    name: "Regina Tania",
+    role: "Architectural design specialist",
+    picture: regina,
     description: "",
   },
   {
@@ -15,17 +57,5 @@ export const membersList: IMember[] = [
     picture: welvin,
     description:
       "A passionate programmer who enjoys taking complex problems and turning them into simple and efficient solutions using code. Welvin is currently a Computer Science student at the University of Hong Kong.",
-  },
-  {
-    name: "person C",
-    role: "person C's role",
-    picture: placeholder,
-    description: "",
-  },
-  {
-    name: "person D",
-    role: "person D's role",
-    picture: placeholder,
-    description: "",
   },
 ];
